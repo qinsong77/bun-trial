4309b67fb7ffeac39c1d3a74a8a38962873f550c
notend <xyqinsong@163.com>
2024-05-12 00:27:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bun Trial
+
+Trial demos for Bun
+
+* [AsyncLocalStorage](https://www.youtube.com/watch?v=JejwWxhsfZw)
---
+++ demos/AsyncLocalStorage/user.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+import { cookies } from './index'
+
+export function User() {
+  const cookieStore = cookies()
+
+  return (
+    <body>
+    <h1>cookies()</h1>
+    <p>This reads <code>name</code> from cookies</p>
+    <p>Names: <code>{cookieStore?.name || 'No name'}</code></p>
+    </body>
+  )
+}
